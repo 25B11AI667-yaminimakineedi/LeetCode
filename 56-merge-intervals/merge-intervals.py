459cd248041e24ebacce1f12cd52bef42dac03f1
Yamini Makineedi <yaminimakineedi2008@gmail.com>
2026-08-30 14:44:14 +0530

    Time: 15 ms (7.02%) | Memory: 22.4 MB (71.75%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        i=0
+        while i < len(intervals)-1:
+            if intervals[i][1]<intervals[i+1][0]:
+                i+=1
+            else:
+                merged=[intervals[i][0],max(intervals[i][1],intervals[i+1][1])]
+                del intervals[i+1]
+                intervals[i]=merged
+        return intervals
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
