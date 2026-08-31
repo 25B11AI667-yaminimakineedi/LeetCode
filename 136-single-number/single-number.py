dd85a5685623b063709e8683f3d20b1bef317116
Yamini Makineedi <yaminimakineedi2008@gmail.com>
2026-08-31 14:45:45 +0530

    Time: 11 ms (16.07%) | Memory: 21.7 MB (10.63%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dic={}
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                dic[nums[i]]+=1
+            else:
+                dic[nums[i]]=1
+        for i in dic.keys():
+            if dic[i]==1:
+                return i
