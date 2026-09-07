fba808dcf5cac6fd53260ce7bc9cc28613ba323e
Yamini Makineedi <yaminimakineedi2008@gmail.com>
2026-09-07 11:40:31 +0530

    Time: 27 ms (23.65%) | Memory: 19.8 MB (35.31%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result=[]
+        board=[["."]*n for _ in range(n)]
+        def issafe(row,col):
+            for j in range(col):
+                if board[row][j]=="Q":
+                    return False
+            i,j=row-1,col-1
+            while i>=0 and j>=0:
+                if board[i][j]=="Q":
+                    return False
+                i-=1
+                j-=1
+            i,j=row+1,col-1
+            while i<n and j>=0:
+                if board[i][j]=="Q":
+                    return False
+                i+=1
+                j-=1
+            return True
+        def sol_queens(col):
+            if col>=n:
+                result.append(["".join(row) for row in board])
+                return
+            for row in range(n):
+                if(issafe(row,col)):
+                    board[row][col]="Q"
+                    sol_queens(col+1)
+                    board[row][col]="."
+        sol_queens(0)
+        return result        
