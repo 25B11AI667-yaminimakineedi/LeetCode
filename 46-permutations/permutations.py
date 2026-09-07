526900b068c6c05e81ecfac7d25876b85a55d065
Yamini Makineedi <yaminimakineedi2008@gmail.com>
2026-09-07 09:44:24 +0530

    Time: 3 ms (31.47%) | Memory: 19.6 MB (10.09%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result=[]
+        def swap_digits(i):
+            if i==len(nums):
+                result.append(nums.copy())
+                return 
+            for j in range(i,len(nums)):
+                nums[i],nums[j]=nums[j],nums[i]
+                swap_digits(i+1)
+                nums[i],nums[j]=nums[j],nums[i]
+        swap_digits(0)
+        return result
+
+
+
