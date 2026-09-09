3536107258e6e9444254a70b6ac081055b84cdd8
Yamini Makineedi <yaminimakineedi2008@gmail.com>
2026-09-09 15:16:08 +0530

    Time: 2 ms (79.33%) | Memory: 19.4 MB (35.47%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def isvalid(board,row,col,c):
+            for i in range(9):
+                if board[row][i]==c:
+                    return False
+                if board[i][col]==c:
+                    return False
+                if board[3*(row//3)+i//3][3*(col//3)+i%3]==c:
+                    return False
+            return True
+        for i in  range(9):
+            for j in range(9):
+                if board[i][j]!=".":
+                    c=board[i][j]
+                    board[i][j]="."
+                    if  not isvalid(board,i,j,c):
+                        return False
+                    board[i][j]=c
+        return True
+        
+                   
