af9db06d966e255ffd057b07ee0b1ef8045af800
Yamini Makineedi <yaminimakineedi2008@gmail.com>
2026-09-18 21:24:59 +0530

    Time: 3 ms (51.30%) | Memory: 19.3 MB (24.81%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,9 +1,20 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        count_s=Counter(s)
-        count_t=Counter(t)
+        hashh_s={}
+        hashh_t={}
+        for i in s:
+            if i  in hashh_s:
+                hashh_s[i]+=1
+            else:
+                hashh_s[i]=1
         for i in t:
-            if count_t[i]>count_s[i]:
+            if i  in hashh_t:
+                hashh_t[i]+=1
+            else:
+                hashh_t[i]=1
+        for i in t:
+            if hashh_t[i]>hashh_s.get(i,0):
                 return i
+
             
             
